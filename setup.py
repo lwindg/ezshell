822a589a7ac3a0cad3813dffb90bcf839c4848d7
Aeluin Chen <aeluin.chen@moxa.com>
2014-10-27 17:53:31 +0800

    Add files for unittest (not implement yet) and installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="ezshell",
+      version="0.1",
+      description="An easy to use shell library.",
+      url="https://github.com/Sanji-IO",
+      author="Sanji Team",
+      author_email="sanji@moxa.com",
+      license="MOXA",
+      packages=["ezshell"],
+      zip_safe=False
+      )
